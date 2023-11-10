494b8d41248653c162c362b01f61d592eedfff9b
Brian Etaghene <joveee05@gmail.com>
2023-11-10 01:26:09 +0100

    [Added] route protection. Block/unblock users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const auth = require('../middlewares/auth');
 
 router.post('/register', userController.createUser);
 
 router.post('/login', userController.logIn);
+
+router.use(auth.protect);
 
 router.get('/all_users', userController.getAllUsers);
 
@@ -13,5 +16,9 @@ router.get('/:id', userController.getUser);
 router.patch('/:id', userController.updateUser);
 
 router.delete('/:id', userController.deleteUser);
+
+router.patch('/block-user/:id', auth.isAdmin, userController.blockUser);
+
+router.patch('/unblock-user/:id', auth.isAdmin, userController.unblockUser);
 
 module.exports = router;
