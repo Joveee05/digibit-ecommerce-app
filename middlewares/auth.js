ea4a84844e4d957255528b6d4483421c0be78adf
Brian Etaghene <joveee05@gmail.com>
2023-11-10 01:24:38 +0100

    [Added] jwt authorization

---
+++ middlewares/auth.js
@@ -0,0 +1,32 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (req?.headers?.authorization?.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+    try {
+      if (token) {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded?.id);
+        req.user = user;
+        next();
+      }
+    } catch (error) {
+      throw new Error('Not authorized. Token expired. Please log in again');
+    }
+  } else {
+    throw new Error('No token attached to header');
+  }
+});
+
+exports.isAdmin = asyncHandler(async (req, res, next) => {
+  const { email } = req.user;
+  const adminUser = await User.findOne({ email });
+  if (adminUser.role !== 'admin') {
+    throw new Error('You are not an admin');
+  } else {
+    next();
+  }
+});
