4489e9086814f16d0dc069875dcfaeabe4b203d9
Brian Etaghene <joveee05@gmail.com>
2023-11-12 22:52:05 +0100

    [Added] refreshToken handler function

---
+++ config/refreshToken.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const generateRefreshToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN,
+  });
+
+exports.sendAccessToken = async (user, res) => {
+  const id = user._id;
+  const refreshToken = generateRefreshToken(id);
+  const updateUser = await User.findByIdAndUpdate(
+    id,
+    { refreshToken: refreshToken },
+    { new: true }
+  );
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('refreshToken', refreshToken, cookieOptions);
+};
