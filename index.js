ed5a6e838997a3d87d6faf17d14729eb593ab9d0
Brian Etaghene <joveee05@gmail.com>
2023-11-15 16:43:41 +0100

    [Added] morgan and products routes

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const app = express();
 const dotenv = require('dotenv').config();
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const authRouter = require('./routes/authRoutes');
+const productRouter = require('./routes/productRoutes');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 4000;
 
 dbConnect();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 app.use('/api/users', authRouter);
+app.use('/api/products', productRouter);
 
 app.use(notFound);
 app.use(errorHandler);
