c46a1871c78f8af38ce34aaf9aa9bd5132d4210d
Brian Etaghene <joveee05@gmail.com>
2023-11-15 16:38:06 +0100

    [Added] products model

---
+++ models/productModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowerCase: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    sold: {
+      type: Boolean,
+      default: false,
+    },
+    images: {
+      type: Array,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    ratings: [
+      {
+        star: Number,
+        postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      },
+    ],
+  },
+  { timestamps: true, versionKey: false }
+);
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
