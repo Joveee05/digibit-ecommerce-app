27aa33e37f2f8098626e5a166707c4f8b50b8d56
Brian Etaghene <joveee05@gmail.com>
2023-11-26 13:32:52 +0100

    [Added] API Features

---
+++ utils/apiFeatures.js
@@ -0,0 +1,38 @@
+const Product = require('../models/productModel');
+
+async function APIFeatures(req) {
+  const queryObj = { ...req.query };
+  const excludedFields = ['page', 'sort', 'limit', 'fields'];
+  excludedFields.forEach((el) => delete queryObj[el]);
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+  let query = Product.find(JSON.parse(queryStr));
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    query = query.select(fields);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 100;
+  const skip = (page - 1) * limit;
+
+  query = query.skip(skip).limit(limit);
+  if (req.query.page) {
+    const productCount = await Product.countDocuments();
+    if (skip >= productCount) throw new Error("Page doesn't exist");
+  }
+
+  return query;
+}
+module.exports = { APIFeatures };
