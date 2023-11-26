f486834605139f6a0d2298f6fdb2a763a4f2bf2f
Brian Etaghene <joveee05@gmail.com>
2023-11-26 13:37:56 +0100

    [Added] reset password route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,14 +15,16 @@ router.get('/all_users', userController.getAllUsers);
 
 router.get('/refresh', userController.handleRefreshToken);
 
-router.get('/:id', userController.getUser);
-
-router.patch('/:id', userController.updateUser);
-
-router.delete('/:id', userController.deleteUser);
+router.patch('/reset-password', userController.updatePassword);
 
 router.patch('/block-user/:id', auth.isAdmin, userController.blockUser);
 
 router.patch('/unblock-user/:id', auth.isAdmin, userController.unblockUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
